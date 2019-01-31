25098d44d967cfd9f15aa7fc3963eaab78e2926c
Dan Stroot <dan.stroot@gmail.com>
2019-01-31 10:58:40 -0800

    clean tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 describe("App", () => {
   it("it should render", () => {
     const div = document.createElement("div");
-    ReactDOM.render(
-      <Router>
-        <App />
-      </Router>,
-      div
-    );
+    ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
--- src/components/NavBar/index.test.js
+++ src/components/NavBar/index.test.js
@@ -1,15 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
 
 describe("NavBar", () => {
   it("it should render", () => {
-    const component = renderer.create(
-      <Router>
-        <NavBar />
-      </Router>
-    );
+    const component = renderer.create(<NavBar />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
